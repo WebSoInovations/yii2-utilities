02cdd10e8f9497092990d2ab10c58e77ef6bca89
Alex <acruz@softlogik.mx>
2018-02-02 09:50:15 -0600

    Toggle button widget

---
+++ widgets/ToggleButton.php
@@ -0,0 +1,32 @@
+<?php
+namespace app\components\widgets;
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class ToggleButton extends Widget
+{
+	public $url;
+
+	public $attribute;
+
+	public $value;
+
+	public $grid;
+
+	private $button;
+
+	public function init()
+	{
+		parent::init();
+		$this->button = Html::a(Html::tag('span', '', [
+			'class' => $this->value == 1 ? 'fa fa-check-circle text-success' : 'fa fa-times-circle text-danger'
+		]), (empty($this->url) ? '#' : $this->url), [
+			'onclick' => "return $(this).button.toggle('{$this->url}', '{$this->grid}');"
+		]);
+	}
+
+	public function run()
+	{
+		return Html::decode($this->button);
+	}
+}
